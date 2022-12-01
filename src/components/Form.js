5d8a178db4aa924161319b0b55ba4a4287b19e22
Brayan Stiven Cifuentes <1704stiven@gmail.com>
2022-12-01 09:59:53 -0500

    Finalizacion tercera parte

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,85 @@
-import React, {useState} from 'react';
-import {Text, Modal, Button, View} from 'react-native';
+import React from 'react';
+import {
+  Text,
+  Modal,
+  View,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 
-export const Form=({modalVisible, newUser})=>{
-    
-    return (
-        <Modal animationType="slide" visible={modalVisible}>
-<Text>Formulario de registro</Text>
-<Button title="Aceptar" onPress={newUser}></Button>
-        </Modal>
-    );
-}
+export const Form = ({modalVisible}) => {
+  return (
+    <Modal animationType="slide" visible={modalVisible}>
+      <SafeAreaView style={styles.content}>
+        <ScrollView>
+          <Text style={styles.title}>
+            Nuevo {''}
+            <Text style={styles.boldTitle}>Usuario</Text>
+          </Text>
+          <View style={styles.text_input}>
+            <Text style={styles.label}>Nombre</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType=""
+              placeholder="Nombre"
+              placeholderTextColor={'#000000'}
+            />
+
+            <Text style={styles.label}>Correo</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="email-address"
+              placeholderTextColor={'#000000'}
+            />
+
+            <Text style={styles.label}>Celular</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="phone-pad"
+              placeholderTextColor={'#000000'}
+            />
+
+            <Text style={styles.label}>Nombre</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType=""
+              placeholderTextColor={'#000000'}
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+    content:{
+        backgroundColor:'#0069a3',
+        flex:1,
+    },
+    title: {
+        fontSize:20,
+        fontWeight:'600',
+        textAlign:'center',
+        marginTop:20,
+        marginEnd:20,
+        color:'#FFF',
+    },
+    boldTitle:{
+        fontWeight:'700',
+    },
+    label:{
+        color:'#FFF',
+        marginBottom:10,
+        marginTop:15,
+        fontSize:15,
+        fontWeight:'600',
+    },
+    text_input:{
+        marginTop:40,
+        marginHorizontal:30,
+        marginBottom:10,
+    }
+})
